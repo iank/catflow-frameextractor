70d9153b4df100ecb6cda5629fcb223fbe0cd44b
Ian Kilgore <iank@iank.org>
2023-06-18 03:44:52 +0000

    Bump version to 0.5.5-dev

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="frameextractor",
-    version="0.5.4",
+    version="0.5.5-dev",
     packages=find_packages(),
     install_requires=required,
     # For torch+cpu
